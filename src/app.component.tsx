ca5b479d91abb9bd26ccae325f022259cfd0f593
Daniele Tortora <contact@danieletortora.com>
2022-11-23 09:05:52 +0100

    init repo

---
+++ src/app.component.tsx
@@ -0,0 +1,9 @@
+import { RepositoryViewer } from "./repository-viewer/repository-viewer.component";
+
+const App = () => (
+  <div>
+    <RepositoryViewer />
+  </div>
+);
+
+export default App;
---
+++ src/repository-viewer/data-access/repository.service.ts
@@ -0,0 +1,7 @@
+class RepositoryService {
+  find() {}
+}
+
+const repositoryService = new RepositoryService();
+
+export { repositoryService };
