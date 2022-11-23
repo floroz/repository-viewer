bfb7140744dec172f8e66814a089d8237c26804d
Daniele Tortora <daniele.tortora@tundra.com>
2022-11-23 09:01:02 +0000

    wip connect env via sandbox

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -1,9 +1,30 @@
+import { useEffect } from "react";
 import { RepositoryViewer } from "./repository-viewer/repository-viewer.component";
+import { githubClient } from "./shared/data-access/github-client.service";
 
-const App = () => (
-  <div>
-    <RepositoryViewer />
-  </div>
-);
+const token = import.meta.env['GITHUB_API_TOKEN']
+
+const App = () => {
+  useEffect(() => {
+    const headers = new Headers({});
+    headers.append("Accept", "application/json");
+    headers.append(
+      "Authorization",
+      `Bearer ${token}`
+    );
+
+    fetch("https://api.github.com/repos/floroz/repository-viewer/branches", {
+      headers,
+    })
+      .then((res) => res.json())
+      .then(console.log);
+  }, [])
+
+  return (
+    <div>
+      <RepositoryViewer />
+    </div>
+  );
+};
 
 export default App;
